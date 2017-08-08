c878e8508382807e4ec6f7389a60b6bc31d62f56
fulai <986734037@qq.com>
2017-08-08 22:19:27 +0800

    'zhushi'

---
+++ picasso-sample/src/main/java/com/example/picasso/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.picasso;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView picImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Picasso.with().setLoggingEnabled(true);
+        Picasso.with().setIndicatorsEnabled(true);
+        picImg = (ImageView) findViewById(R.id.pic_img);
+    }
+
+    public void onClick(View view) {
+        /*Intent intent = new Intent(this, SampleGridViewActivity.class);
+        startActivity(intent);*/
+        String url = "http://i.imgur.com/CqmBjo5.jpg";
+        Picasso.with()
+                .load(url)
+                .placeholder(R.drawable.placeholder) //
+                .error(R.drawable.error) //
+                .fit() //
+                .tag(this) //
+                .into(picImg);
+    }
+}
